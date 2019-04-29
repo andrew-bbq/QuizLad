8bfa795dc9e99055456b21c23e07a12300584417
Adam Nygard <anygard@wisc.edu>
2019-04-29 18:53:40 -0500

    fix null pointer exception

--- application/Main.java
+++ application/Main.java
@@ -43,77 +43,75 @@ public class Main extends Application {
   public static void main(String[] args) {
     launch(args);
   }
-  
-  public Scene home () {
-	  // Creation of buttons and boxes
-      VBox vungryBox = new VBox();
-      HBox hungryBox1 = new HBox();
-      HBox hungryBox2 = new HBox();
-      HBox hungryBox3 = new HBox();
-      Text topicLabel = new Text("Topics ");
-      Button takeQuiz = new Button("Take Quiz");
-      Button addNewQuestion = new Button("Add New Question");
-      Button importBooks = new Button("Import JSON");
-      Button export = new Button("Export");
-      TextField numQuestions = new TextField();
-      numQuestions.setMaxWidth(50);
-      Text placeHolderNum = new Text("Number of Questions: ");
 
-      // Create comboBox with possible topics
-      ObservableList<String> topicList = FXCollections.observableArrayList(questionList.keySet());
-      Collections.sort(topicList);
-      ComboBox<String> topics = new ComboBox<String>();
-      topics.setItems(topicList);
+  public Scene home() {
+    // Creation of buttons and boxes
+    VBox vungryBox = new VBox();
+    HBox hungryBox1 = new HBox();
+    HBox hungryBox2 = new HBox();
+    HBox hungryBox3 = new HBox();
+
+    Text topicLabel = new Text("Topics ");
+    Button takeQuiz = new Button("Take Quiz");
+    Button addNewQuestion = new Button("Add New Question");
+    Button importBooks = new Button("Import JSON");
+    Button export = new Button("Export");
+    TextField numQuestions = new TextField();
+    Text placeHolderNum = new Text("Number of Questions: ");
+
+    // Create comboBox with possible topics
+    ObservableList<String> topicList = FXCollections.observableArrayList(questionList.keySet());
+    Collections.sort(topicList);
+    ComboBox<String> topics = new ComboBox<String>();
+    topics.setItems(topicList);
+    if (!topicList.isEmpty())
       topics.getSelectionModel().selectFirst();
 
-      // Display questions for default topic
-      ArrayList<Question> topicQuestions = questionList.get(topics.getValue());
-      ObservableList<String> questionTitles = FXCollections.observableArrayList();
+    // Display questions for default topic
+    ArrayList<Question> topicQuestions = questionList.get(topics.getValue());
+    ObservableList<String> questionTitles = FXCollections.observableArrayList();
+    if (topicQuestions != null) {
       for (Question q : topicQuestions) {
         questionTitles.add(q.getQuestionTitle());
       }
-      ListView<String> list = new ListView<String>(questionTitles);
+    }
+    ListView<String> list = new ListView<String>(questionTitles);
 
-      // CSS styling for objects
-      /*hungryBox1.setStyle("-fx-border-color:black;");
-      hungryBox1.setStyle("-fx-border-radius:5;");
-      takeQuiz.setStyle("-fx-font-size:20px;");
-      placeHolderNum.setStyle("-fx-font-size:21px;");*/
-      
-      takeQuiz.getStyleClass().addAll("custom-button", "basic-text");
-      addNewQuestion.getStyleClass().addAll("custom-button", "basic-text");
-      importBooks.getStyleClass().addAll("custom-button", "basic-text");
-      export.getStyleClass().addAll("custom-button", "basic-text");
-      topics.getStyleClass().addAll("custom-combo", "basic-text");
-      placeHolderNum.getStyleClass().addAll("basic-text");
-      placeHolderNum.setFont(Font.font("Letter Gothic"));
-      placeHolderNum.setFill(Color.rgb(13,61,137));
-      numQuestions.getStyleClass().addAll("custom-textfield");
-      list.getStyleClass().addAll("custom-list", "basic-text");     
-      topicLabel.getStyleClass().addAll("basic-text", "text-padding-top");
-      topicLabel.setFill(Color.rgb(13,61,137));
-      HBox.setMargin(topicLabel, new Insets(8,0,0,0));
-      HBox.setMargin(importBooks, new Insets(0,10,0,0));
-      
-      hungryBox1.setPadding(new Insets(10, 10, 0, 10));
-      hungryBox2.setPadding(new Insets(10, 10, 0, 10));
-      hungryBox3.setPadding(new Insets(10, 10, 10, 10));
+    numQuestions.setMaxWidth(50);
+    takeQuiz.getStyleClass().addAll("custom-button", "basic-text");
+    addNewQuestion.getStyleClass().addAll("custom-button", "basic-text");
+    importBooks.getStyleClass().addAll("custom-button", "basic-text");
+    export.getStyleClass().addAll("custom-button", "basic-text");
+    topics.getStyleClass().addAll("custom-combo", "basic-text");
+    placeHolderNum.getStyleClass().addAll("basic-text");
+    placeHolderNum.setFont(Font.font("Letter Gothic"));
+    placeHolderNum.setFill(Color.rgb(13, 61, 137));
+    numQuestions.getStyleClass().addAll("custom-textfield");
+    list.getStyleClass().addAll("custom-list", "basic-text");
+    topicLabel.getStyleClass().addAll("basic-text", "text-padding-top");
+    topicLabel.setFill(Color.rgb(13, 61, 137));
+    HBox.setMargin(topicLabel, new Insets(8, 0, 0, 0));
+    HBox.setMargin(importBooks, new Insets(0, 10, 0, 0));
 
-      topics.setMinSize(Button.USE_PREF_SIZE, Button.USE_PREF_SIZE);
-      final Pane spacer = new Pane();
-      HBox.setHgrow(spacer, Priority.ALWAYS);
-      final Pane spacer2 = new Pane();
-      HBox.setHgrow(spacer2, Priority.ALWAYS);
-      spacer.setMinSize(10, 1);
-      takeQuiz.setMinSize(Button.USE_PREF_SIZE, Button.USE_PREF_SIZE);
+    hungryBox1.setPadding(new Insets(10, 10, 0, 10));
+    hungryBox2.setPadding(new Insets(10, 10, 0, 10));
+    hungryBox3.setPadding(new Insets(10, 10, 10, 10));
 
-      // Add all objects to scene and display
-      hungryBox1.getChildren().addAll(topicLabel, topics, spacer, takeQuiz);
-      hungryBox2.getChildren().addAll(addNewQuestion, spacer2, importBooks, export);
-      hungryBox3.getChildren().addAll(placeHolderNum, numQuestions);
-      vungryBox.getChildren().addAll(hungryBox1, hungryBox2, hungryBox3, list);
-      Scene scene = new Scene(vungryBox, 800, 600);
-      scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-      return scene;
+    topics.setMinSize(Button.USE_PREF_SIZE, Button.USE_PREF_SIZE);
+    final Pane spacer = new Pane();
+    HBox.setHgrow(spacer, Priority.ALWAYS);
+    final Pane spacer2 = new Pane();
+    HBox.setHgrow(spacer2, Priority.ALWAYS);
+    spacer.setMinSize(10, 1);
+    takeQuiz.setMinSize(Button.USE_PREF_SIZE, Button.USE_PREF_SIZE);
+
+    // Add all objects to scene and display
+    hungryBox1.getChildren().addAll(topicLabel, topics, spacer, takeQuiz);
+    hungryBox2.getChildren().addAll(addNewQuestion, spacer2, importBooks, export);
+    hungryBox3.getChildren().addAll(placeHolderNum, numQuestions);
+    vungryBox.getChildren().addAll(hungryBox1, hungryBox2, hungryBox3, list);
+    Scene scene = new Scene(vungryBox, 800, 600);
+    scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+    return scene;
   }
 }
